4dc44906b39456b669f90f7b7db6cdcc2c98f72e
DEASTR121 <carlog101203@gmail.com>
2025-02-11 20:02:26 -0500

    comit panel lasted

--- config/static/js/onInfo.js
+++ config/static/js/onInfo.js
@@ -1,5 +1,5 @@
 //TODO CAMBIAR EL URL DEL SOCKET IO POR EL LINK DEL PANEL DE ADMINISTRACIÓN
-let socket = io.connect("http://localhost:5000/");
+let socket = io.connect("https://rectar.onrender.com/");
 
 function generateTableRow(data) {
   return `
